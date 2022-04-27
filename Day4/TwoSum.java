741dd53828431789099c5207cbc51e7f2df62f9e
sanket_ghormode <sanghormode2@gmail.com>
2022-04-27 18:31:24 +0530

    .'

--- Day4/TwoSum.java
+++ Day4/TwoSum.java
@@ -4,7 +4,7 @@ public class TwoSum {
     public static void main(String[] args) {
         int[] nums = {3,2,4};
         int target = 6;
-        System.out.println(Arrays.toString(twoSum1(nums, target)));
+        System.out.println(Arrays.toString(twoSum2(nums, target)));
     }
     static int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
@@ -25,6 +25,7 @@ public class TwoSum {
         while (i < j) {
             if (arr[i]+arr[j] == target) {
                 n1 = arr[i]; n2 = arr[j];
+                break;
             }
             if (arr[i]+arr[j] > target) {
                 j--;
@@ -33,16 +34,30 @@ public class TwoSum {
                 i++;
             }
         }
+        i=-1; j=-1;
         for (int j2 = 0; j2 < arr.length; j2++) {
-            if (n1 == nums[j2]) {
+            if (n1 == nums[j2] && i==-1) {
                 i = j2;
             }
-            else if (n2 == nums[j2]) {
+            else if (n2 == nums[j2] && j==-1) {
                 j = j2;
             }
         }
-        System.out.println(Arrays.toString(arr));
-        System.out.println(Arrays.toString(nums));
+        // System.out.println(Arrays.toString(arr));
+        // System.out.println(Arrays.toString(nums));
         return new int[]{i, j};
+    }
+    static int[] twoSum2(int[] nums, int target){
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            else {
+                map.put(nums[i], i);
+            }
+        }
+        return new int[]{-1, -1};
     }
 }
